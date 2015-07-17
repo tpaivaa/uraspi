7f1807c83dc98b2bbd38d82c25c99a5a15d5e6f4
Tomi Lindström <tomislindstrom@gmail.com>
2015-07-17 16:21:04 +0300

    get metodi

--- app.js
+++ app.js
@@ -9,7 +9,11 @@ var jsonParser = bodyParser.json()
 // create application/x-www-form-urlencoded parser 
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-app.post('/login', urlencodedParser, function (req, res) {
+app.get('/', function (req, res, next) {
+	res.send('<h2> uRaspi </h2>');
+})
+
+app.post('/login', urlencodedParser, function (req, res, next) {
   if (!req.body) return res.sendStatus(400)
   res.send('welcome, ' + req.body.username)
 })
