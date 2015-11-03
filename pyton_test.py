d3511be3309899440c4aa48b7d8f386696fbddfd
Tomi Hakkarainen <tpaivaa@gmail.com>
2015-11-03 22:48:47 +0200

    Update pyton_test.py

--- pyton_test.py
+++ pyton_test.py
@@ -7,11 +7,11 @@ GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 GPIO.setup(17, GPIO.OUT)
 GPIO.setup(22, GPIO.OUT)
-
+GPIO.setup(23, GPIO.OUT)
 
 def verantavalo(channel):
     time.sleep(0.1)
-    if GPIO.input(2) != GPIO.HIGH:
+    if GPIO.input(2) != GPIO.LOW:
         return
     if(GPIO.input(17) == 0):
         GPIO.output(17,1)
@@ -20,14 +20,18 @@ def verantavalo(channel):
 
 def ulkovalo(channel):
     time.sleep(0.1)
-    if GPIO.input(7) != GPIO.HIGH:
+    if GPIO.input(7) != GPIO.LOW:
         return
     if(GPIO.input(22) == 0):
         GPIO.output(22,1)
     else:
         GPIO.output(22,0)
+    if(GPIO.input(23) == 0):
+        GPIO.output(23,1)
+    else:
+        GPIO.output(23,0)
 
-GPIO.add_event_detect(2, GPIO.RISING, callback=verantavalo)
+GPIO.add_event_detect(2, GPIO.FALLING, callback=verantavalo, bouncetime=1000)
 
-GPIO.add_event_detect(7, GPIO.RISING, callback=ulkovalo)
+GPIO.add_event_detect(7, GPIO.FALLING, callback=ulkovalo, bouncetime=1000)
 
